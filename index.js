b18ada60a186987a13ea3fe2836766b071ae423d
billymwangi <billy.mwangi@student.moringaschool.com>
2022-06-02 12:22:55 +0300

    Done.

---
+++ index.js
@@ -0,0 +1,20 @@
+function add(a, b) {
+    return a + b;
+  }
+function subtract(a, b) {
+    return a - b;
+  }
+function multiply(a, b) {
+    return a * b;
+  }
+function divide(a, b) {
+    return a/ b;
+  }
+function increment(n) {
+    return n +=n 
+}
+function decrement (n) {
+    return n -= 4
+}
+ParseInt("2", 10);
+parseInt("nonsense!", 10);
